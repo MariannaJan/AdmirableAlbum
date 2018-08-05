cfc0871005e0026d4c5404c21411db2d3e463d1d
Marianna <janczak.mariannal@gmail.com>
2018-08-05 17:49:18 +0200

    photos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Album/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Album from './components/Album.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Album
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+
 </style>
---
+++ src/components/Album.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class>
+        <a href="#">
+        <img src="@/assets/Photos/park1.jpg" />
+        <p>Some text <span>here</span>.</p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Album',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+        img {
+            width: 50%;
+            height: 50%;
+            filter: sepia(90%) brightness(0.6) blur(2px);
+            }
+        a:hover img {
+            filter: none;
+            }
+        a:hover span {
+            font-weight: bold;
+        }
+</style>
